e87c13b7c2e871561d9710bb43527178334c5818
jmcoder1 <jmcoder@outlook.com>
2017-11-19 15:09:21 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+var num1;
+var num2;
+var guess;
+var answer;
+var reponse;
+
+$(document).ready(new function() {
+    num1 = document.getElementById("num1");
+    num2 = document.getElementById("num2");
+    guess = document.getElementById("inputfield");
+    response = document.getElementById("response")
+    
+    $("#submitbutton").click(function() {
+              checkAnswer();              
+    });
+                             
+    setNumbers();
+});
+
+function setNumbers() {
+    num1.innerHTML = Math.floor(Math.random() * 10) + 1;
+    num2.innerHTML = Math.floor(Math.random() * 10) + 1;
+}
+
+function checkAnswer() {
+    var n1 = parseInt(num1.innerHTML);
+    var n2 = parseInt(num2.innerHTML);
+    answer = n1 * n2;
+    
+    if(parseInt(guess.value) == answer) {
+        response.innerHTML = "Correct";
+        response.style.color = "lawngreen";
+        setNumbers();
+    } else {
+        response.innerHTML = "Incorrect";
+        response.style.color = "red";
+    }
+}
